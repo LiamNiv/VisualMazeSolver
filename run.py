73f10c45772fd52d5896700677ac6925592d7dce
Liam Niv <liamniv@gmail.com>
2024-10-24 21:04:47 +0300

    make screen management by functions + keep aspect ratio a constant

--- run.py
+++ run.py
@@ -4,20 +4,38 @@ from sys import exit
 # Constants
 DEFAULT_DISPLAY_WIDTH = 1280
 DEFAULT_DISPLAY_HEIGHT = 720
+ASPECT_RATIO = 16 / 9
 
-def main():
-    pygame.init()
+
+def init_display():
     screen = pygame.display.set_mode((DEFAULT_DISPLAY_WIDTH, DEFAULT_DISPLAY_HEIGHT), pygame.RESIZABLE)
     pygame.display.set_caption('VisualMazeSolver')
-    clock = pygame.time.Clock()
+    return screen
 
-
-    while True:
-        for event in pygame.event.get():
+def handle_window_events(screen):
+    for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
             elif event.type == pygame.VIDEORESIZE:
-                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                width, height = event.size
+                # Keep the aspect ratio
+                # screen too wide
+                if width / height > ASPECT_RATIO:
+                    width = int(height * ASPECT_RATIO)
+                # screen too tall
+                else:
+                    height = int(width / ASPECT_RATIO)
+                screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+
+
+def main():
+
+    pygame.init()
+    screen = init_display()
+    clock = pygame.time.Clock()
+
+    while True:
+        screen = handle_window_events(screen)
 
         pygame.display.update()
         clock.tick(60)  # 60 tick per second
