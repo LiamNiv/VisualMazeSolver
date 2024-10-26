3262436b4a38c1154db70b6e364244295a7b588b
Liam Niv <liamniv@gmail.com>
2024-10-26 14:11:11 +0300

    add "is_init" var for each state of the program

--- state.py
+++ state.py
@@ -9,15 +9,23 @@ class State(Enum):
 
 class StateManager:
     def __init__(self):
-        self.state = State.MAZECREATION # only for now, should be MAINMENU
-        self.is_maze_created = False
+        self.current_state = State.MAZECREATION # only for now, should be MAINMENU
+        self.initialized = {
+            State.MAINMENU: False,
+            State.MAZECREATION: False,
+            State.MAZESOLVING: False,
+            State.FINISHED: False
+        }
+
+    def set_current_state_initialized(self, state):
+        self.initialized[self.current_state] = True
 
     def set_state(self, state):
 
         if not isinstance(state, State):
             raise ValueError("State must be of type State")
 
-        self.state = state
+        self.current_state = state
 
     def get_state(self):
-        return self.state
+        return self.current_state
